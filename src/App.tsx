95e1337428492c0ece61b7b57d5064e5130676de
K4RU3 <pzKARUzq2@gmail.com>
2025-02-06 12:22:30 +0900

    use api

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getData, currentType } from "./Api"
 import Dashboard from "./Dashboard";
-import Login from "./Login";
-import { useState, useEffect } from "react";
 
 export default function App() {
-    const [username, setUsername] = useState<string>("");
+    const [isLoading, setLoading] = useState<boolean>(true);
+    const [data, setData] = useState<currentType>({});
 
     useEffect(() => {
-    }, [])
+        (async () => {
+            let data = await getData();
+            setData(data);
+            setLoading(false);
+        })()
+    }, []);
 
     return <>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login login={[username, setUsername]} />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-        </Router>
+        {
+            isLoading
+            ? <p>Loading...</p>
+            : <Dashboard data={data} />
+        }
     </>;
 }
--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,5 +1,18 @@
-export default function Dashboard() {
+import { useState, useEffect } from 'react'
+import { currentType } from './Api'
 
-    return <>
-    </>
+export default function Dashboard({ data }: {data: currentType}) {
+    const retDir = () => window.innerWidth > window.innerHeight ? 'hor' : 'ver';
+    const [dir, setDir] = useState<'ver' | 'hor'>(retDir());
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setDir(retDir());
+        });
+    }, []);
+    return (
+        <div>
+
+        </div>
+    )
 }
